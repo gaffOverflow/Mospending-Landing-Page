1b76563abe8516e4f571e9413bf74f0ae3f42d3c
gaffOverflow <gaffar.ibrahim18@gmail.com>
2023-09-28 12:22:37 +0100

    error fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import NavBar from "./components/nav";
 import Features from "./components/features";
 import Footer from "./components/footer";
@@ -8,34 +5,12 @@ import "./App.css";
 import Hero from "./components/Hero";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <NavBar />
       <Hero />
       <Features />
-      <Footer/>
-      {/* <div className="xl:rounded-none bg-blue-500 rounded-[100px] ">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      <Footer />
     </>
   );
 }
